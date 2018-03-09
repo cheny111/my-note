7172d29b5cb0f404399acc7c65f1ff33a23fa28f
chney111 <1326006791@qq.com>
2018-03-09 20:00:38 +0000

    ddd

---
+++ content/README.md
@@ -0,0 +1,3 @@
+# 前端大宝书
+---
+<img src="http://tu.t-cha.com/ctJDA0JDIxL1RCMTFTak5IcFhYWFhjR1hWJDMkNQ.jpg" alt="封面">
--- scripts/deploy-gh-pages.js
+++ scripts/deploy-gh-pages.js
@@ -5,11 +5,5 @@ var ghpages = require('gh-pages');
 main();
 
 function main() {
-    ghpages.publish('./gh-pages', console.error.bind(console));
+  ghpages.publish('./dist', console.error.bind(console));
 }
-
-//这样，每次书稿有了修改，运行npm run publish
-// ghpages.publish('gh-pages', {
-//   branch: 'gh-pages',
-//   repo: 'git@github.com:zhuangzz/zhuangzz-note.git'
-// }, ()=>{});
